11e85589189ec656744bb6772763576f4158c8e0
Enea Jorgji <jorgji.enea@gmail.cm>
2020-10-18 21:16:02 +0200

    comminting a test file

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 # This is a test file, just to tes git and github
+def test():
+    print("this is a test")
+
+
+def test_2():
+    print("this is a second test")
+
+
+if __name__ == '__main__':
+    test()
+    test_2()
