7add5e231157c7d3b446d8422b484b905e76aec5
Enea Jorgji <jorgji.enea@gmail.cm>
2020-10-21 20:23:50 +0200

    added a print function to the quadratic()

--- 7.2_quadratic.py
+++ 7.2_quadratic.py
@@ -1,7 +1,7 @@
 # A program that computes the real roots of a quadraticequation
 
 def quadratic():
-    pass
+    print("This is just something that needs to be printed.")
 
 if __name__ == '__main__':
     quadratic()
