fea0ec3f3f6e5eb7c000c8ba71e0c1d50adea912
Enea Jorgji <jorgji.enea@gmail.cm>
2020-10-22 21:41:17 +0200

    added file average_1"

--- 7.2_quadratic.py
+++ 7.2_quadratic.py
@@ -1,7 +1,8 @@
-# A program that computes the real roots of a quadraticequation
+# A program that computes the real roots of a quadratic equation
 
 def quadratic():
     print("This is just something that needs to be printed.")
+
 
 if __name__ == '__main__':
     quadratic()
